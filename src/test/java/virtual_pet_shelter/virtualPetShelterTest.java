2d906ed187e5a72d0d79022eaa5a3087b2b2316e
jmackert <jonahbmackert@gmail.com>
2019-09-05 17:58:07 -0400

    Finalized project

--- src/test/java/virtual_pet_shelter/virtualPetShelterTest.java
+++ src/test/java/virtual_pet_shelter/virtualPetShelterTest.java
@@ -1,5 +1,47 @@
 package virtual_pet_shelter;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class virtualPetShelterTest {
 
+	@Test
+	public void ShouldAddPet() {
+		// Arrange
+		VirtualPet underTest = new VirtualPet();
+		//Action
+		String result = underTest.addPet(1);
+		//Assertion
+		assertEquals("1", result);
+	}
+	
+	@Test
+	public void ShouldRemovePet() {
+		// Arrange
+		VirtualPet underTest = new VirtualPet();
+		//Action
+		String result = underTest.removePet(1);
+		//Assertion
+		assertEquals("0", result);
+	}
+	
+	@Test
+	public void ShouldDecreaseHunger() {
+		// Arrange
+		VirtualPet underTest = new VirtualPet();
+		//Action
+		String result = underTest.feedPets(1);
+		//Assertion
+		assertEquals("0", result);
+	}
+	@Test
+	public void ShouldIncreaseTime() {
+		// Arrange
+		VirtualPet underTest = new VirtualPet();
+		//Action
+		String result = underTest.increaseTime(1);
+		//Assertion
+		assertEquals("1", result);
+	}
 }
